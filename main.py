53086809eff1ff675050e27c596127d23dc2a230
WZ一只蚊子 <1447998829@qq.com>
2025-02-09 21:22:35 +0800

    增加更多错误处理，优化大量代码，修改Bugs

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from json import dump, load, dumps
 from datetime import datetime
 import requests, pyperclip, hashlib, re, logging, sys
 
-import logging 
-
 # 禁用所有日志输出 
 logging.getLogger('tensorflow').disabled  = True 
 logging.getLogger('selenium').setLevel(logging.CRITICAL)
@@ -41,7 +39,7 @@ def example_conversion_format(examples: str) -> str:
     :param examples: 原始示例内容 
     :return: 返回转换后的示例格式 
     """
-    pattern  = r"<input>(.*?)</input><output>(.*?)</output>"
+    pattern = r"<input>(.*?)</input><output>(.*?)</output>"
     matches = re.findall(pattern, examples, re.DOTALL)
     formatted_examples = []
     for match in matches:
@@ -56,7 +54,7 @@ def example_conversion_format(examples: str) -> str:
     return out_example
 
 #等待页面上没有任何元素变化
-def is_page_stable(driver: webdriver.Chrome, timeout: int =60, interval: float = 1.5) -> bool:
+def is_page_stable(driver: webdriver.Chrome, timeout: int = 60 * 3, interval: float = 1.5) -> bool:
     """
     等待页面上没有任何元素变化
 
@@ -111,43 +109,96 @@ def configure_AI(driver: webdriver.Chrome, AI_URL: str) -> None:
     配置AI
     '''
     driver.get(AI_URL)
-    find_boby = driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div[2]/section/div[1]/div/div/div[1]/span")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[1]/div/div[2]/div[1]/div[2]/section/div[1]/div/div/div[1]/span").click()
     sleep(0.1)
-    find_boby = driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div[3]/div[4]")
-    find_boby.click()
+    # kimi
+    driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div[3]/div[5]").click()
     sleep(0.1)
-    find_boby = driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div/div/div/div[3]/div[4]")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[2]/div/div/div/div[3]").click()
     sleep(1)
+    
+    # d1eepseek R1  用时太长舍去
+    # driver.find_element(By.XPATH, '//*[@id="nworld-app-modal-container"]/div/div[2]/div/div[2]/div/div/div[1]/div/div/div/div[1]/div[3]/div[2]/span').click()
+    # sleep(0.2)
+    # driver.find_element(By.XPATH, '//*[@id="model_list_wrap_modal"]/div/div/div[1]').click()
+    # sleep(1)
 
 #获取题目+AI话术
 def get_problem_saying(pid: str, notes: str) -> str:
+    global user_data
     '''
     #获取题目+AI话术
     '''
-    problem = requests.get(url = 'http://example.oj/api/get-problem-detail?problemId=' + pid, headers = {'User-Agent': 'Mozilla/7.0 (Windows NT 13.0; Win256; x256) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/514.0.0.0 Safari/987.36'})
+    problem_url = f"{user_data['OJ']['URL']}/api/get-problem-detail?problemId=" + pid
+    # print(problem_url)
+    problem = requests.get(url = problem_url, headers = {'User-Agent': 'Mozilla/7.0 (Windows NT 13.0; Win256; x256) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/514.0.0.0 Safari/987.36'})
     problem = problem.json()
+    # print(problem)
     problem = problem['data']['problem']
-    problem = str('请编写可运行完整程序:' + problem['description'] + '输入:' + problem['input'] + '输出:' + problem['output']).replace("\n",  "").replace("\r", "").replace("<code>", "").replace("</code>", "").replace("<input>", "").replace("</input>", "").replace("<output>", "").replace("</output>", "").replace("\\", "") + str(example_conversion_format(problem['examples']) + '。'  + '帮我编写c++程序，不要有其他赘述，并直接输出程序,代码中不要有注释，可开头直接声明std命名空间。' + notes)
-    return problem
+    problem_str = (
+        f"请编写可运行完整程序:{problem['description']}输入:{problem['input']}输出:{problem['output']}"
+        .replace("\r", "")
+        .replace("<code>", "")
+        .replace("</code>", "")
+        .replace("<input>", "")
+        .replace("</input>", "")
+        .replace("<output>", "")
+        .replace("</output>", "")
+        .replace("\\", "")
+    )
+    example_str = example_conversion_format(problem['examples'])
+    problem_str = (
+        f"{problem_str}。{example_str}。帮我编写c++程序，不要有其他赘述，并直接输出程序,代码中不要有注释，可开头直接声明std命名空间。{notes}"
+        .replace('\n', '')
+    )
+    return problem_str
+
+#复制代码
+def copy_code(driver: webdriver.Chrome, AI_URL: str) -> None:
+    try:
+        divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
+        target_divs = [div for div in divs if "复制" in div.text]
+        if target_divs:
+            last_target_div = target_divs[-1]
+        driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
+        last_target_div.click()
+    except Exception as e:
+        print(Fore.YELLOW + '复制时出错,尝试抢修' + Style.RESET_ALL)
+        try:
+            for i in range(100):
+                driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[2]/button/span/div").click()
+        except Exception as e:
+            try:
+                divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
+                target_divs = [div for div in divs if "复制" in div.text]
+                if target_divs:
+                    last_target_div = target_divs[-1]
+                driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
+                last_target_div.click()
+            except Exception as e:
+                print(Fore.RED + '抢修失败' + str(e) + Style.RESET_ALL)
+                try:
+                    driver.refresh()
+                    driver.implicitly_wait(10)
+                    configure_AI(driver = driver, AI_URL = AI_URL)
+                except Exception as e:
+                    print(Fore.RED + '配置AI时出错:' + str(e) + Style.RESET_ALL)
 
 def training_code() -> None:
     global driver, user_data, options
     driver = webdriver.Chrome(service=Service(user_data['ChromeDriver_path']),  options = options)
     tids = input('请输入训练编号,用逗号隔开:')
     AI_URL = user_data['AI_URL']
-    notes = input('备注:')
+    notes = input('备注:')  #代码后添加的东西
 
     #登录
     #OJ
     username = user_data['OJ']['username']
     password = user_data['OJ']['password']
-    driver.get("http://example.oj/home")
+    driver.get(f"{user_data['OJ']['URL']}/home")
     driver.maximize_window()
     driver.implicitly_wait(10)
-    find_boby = driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary el-button--medium is-round']")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary el-button--medium is-round']").click()  #登录按钮
     find_boby = driver.find_element(By.XPATH,  "//input[@placeholder='用户名']")
     find_boby.click()
     find_boby.send_keys(username)
@@ -155,8 +206,7 @@ def training_code() -> None:
     find_boby.click()
     find_boby.send_keys(password)
     sleep(0.1)
-    find_boby = driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary']")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary']").click()
     sleep(1)
     #获取cookile
     driver.refresh()
@@ -167,9 +217,8 @@ def training_code() -> None:
     for tid in tids.split(','):
         print('start,tid:', str(tid))
         #获取题目id
-        driver.get("http://example.oj/training/" + str(tid))
-        find_boby = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div[3]")
-        find_boby.click()
+        driver.get(f"{user_data['OJ']['URL']}/training/" + str(tid))
+        find_boby = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div[3]").click()
         question_ID = []
         divs = driver.find_elements(By.XPATH, "//div[contains(concat(' ', @class, ' '), ' vxe-cell ') and contains(concat(' ', @class, ' '), ' c--tooltip ') and contains(@style, 'width: 148px;')]")
         #遍历
@@ -190,11 +239,7 @@ def training_code() -> None:
             sleep(0.5)
             find_boby = driver.find_element(By.XPATH, "//textarea[@placeholder='输入任何问题，Enter发送，Shift + Enter 换行']")
             find_boby.click()
-            try:
-                find_boby.send_keys(problem)
-            except Exception as e:
-                print(Fore.RED + '回答问题时出错:' + str(e) + Style.RESET_ALL)
-                continue
+            find_boby.send_keys(problem)
             sleep(1)
             find_boby.send_keys("\n")
             sleep(10)
@@ -202,42 +247,7 @@ def training_code() -> None:
                 print(Fore.RED + "页面不稳定超时" + Style.RESET_ALL)
                 continue
             #复制
-            sleep(1)
-            try:
-                divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
-                target_divs = [div for div in divs if "复制" in div.text]
-                if target_divs:
-                    last_target_div = target_divs[-1]
-                driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
-                last_target_div.click()
-            except Exception as e:
-                print(Fore.RED + '复制时出错:' + str(e) + Style.RESET_ALL)
-                driver.refresh()
-                #配置AI
-                try:
-                    configure_AI(driver, AI_URL)
-                except Exception as e:
-                    print(Fore.RED + '配置AI时出错:' + str(e) + Style.RESET_ALL)
-                    try:
-                        configure_AI(driver, AI_URL)
-                    except Exception as e:
-                        print(Fore.RED + '配置AI时出错,无法配置AI将继续执行:' + str(e) + Style.RESET_ALL)
-                try:
-                    divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
-                    target_divs = [div for div in divs if "复制" in div.text]
-                    if target_divs:
-                        last_target_div = target_divs[-1]
-                    driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
-                    last_target_div.click()
-                except Exception as e:
-                    print(Fore.RED + '复制时出错:' + str(e) + Style.RESET_ALL)
-                    driver.refresh()
-                    #配置AI
-                    try:
-                        configure_AI(driver, AI_URL)
-                    except Exception as e:
-                        print(Fore.RED + '配置AI时出错:' + str(e) + Style.RESET_ALL)
-                    continue
+            copy_code(driver = driver, AI_URL = AI_URL)
             sleep(0.5)
             code = pyperclip.paste()
 
@@ -256,7 +266,7 @@ def training_code() -> None:
                 'pid': i, 
                 'tid': None
             }
-            response = requests.post(url='http://example.oj/api/submit-problem-judge', headers=headers, data=dumps(data))
+            response = requests.post(url = f"{user_data['OJ']['URL']}/api/submit-problem-judge", headers=headers, data=dumps(data))
             if response.status_code != 200:
                 print(Fore.RED + '提交请求异常,status:' + str(response.status_code) + Style.RESET_ALL)
             now = datetime.now()
@@ -270,17 +280,16 @@ def problem_code() -> None:
 
     pids = input('请输入题目ID,用逗号隔开:')
     AI_URL = user_data['AI_URL']
-    notes = input('备注:')
+    notes = input('备注:')  #代码后添加的东西
 
     #登录
     #OJ
     username = user_data['OJ']['username']
     password = user_data['OJ']['password']
-    driver.get("http://example.oj/home")
+    driver.get(f"{user_data['OJ']['URL']}/home")
     driver.maximize_window()
     driver.implicitly_wait(10)
-    find_boby = driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary el-button--medium is-round']")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary el-button--medium is-round']").click()  #登录按钮
     find_boby = driver.find_element(By.XPATH,  "//input[@placeholder='用户名']")
     find_boby.click()
     find_boby.send_keys(username)
@@ -288,9 +297,9 @@ def problem_code() -> None:
     find_boby.click()
     find_boby.send_keys(password)
     sleep(0.1)
-    find_boby = driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary']")
-    find_boby.click()
+    driver.find_element(By.XPATH,  "//button[@class='el-button el-button--primary']").click()
     sleep(1)
+
     #获取cookile
     driver.refresh()
     cookies = driver.get_cookies()
@@ -309,11 +318,7 @@ def problem_code() -> None:
         sleep(0.5)
         find_boby = driver.find_element(By.XPATH, "//textarea[@placeholder='输入任何问题，Enter发送，Shift + Enter 换行']")
         find_boby.click()
-        try:
-            find_boby.send_keys(problem)
-        except Exception as e:
-            print(Fore.RED + '回答问题时出错:' + str(e) + Style.RESET_ALL)
-            continue
+        find_boby.send_keys(problem)
         sleep(1)
         find_boby.send_keys("\n")
         sleep(10)
@@ -321,42 +326,7 @@ def problem_code() -> None:
             print(Fore.RED + "页面不稳定超时" + Style.RESET_ALL)
             continue
         #复制
-        sleep(1)
-        try:
-            divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
-            target_divs = [div for div in divs if "复制" in div.text]
-            if target_divs:
-                last_target_div = target_divs[-1]
-            driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
-            last_target_div.click()
-        except Exception as e:
-            print(Fore.RED + '复制时出错:' + str(e) + Style.RESET_ALL)
-            driver.refresh()
-            #配置AI
-            try:
-                configure_AI(driver, AI_URL)
-            except Exception as e:
-                print(Fore.RED + '配置AI时出错:' + str(e) + Style.RESET_ALL)
-                try:
-                    configure_AI(driver, AI_URL)
-                except Exception as e:
-                    print(Fore.RED + '配置AI时出错,无法配置AI将继续执行:' + str(e) + Style.RESET_ALL)
-            try:
-                divs = driver.find_elements(By.CSS_SELECTOR,  "div[class*='ml-[4px]']")
-                target_divs = [div for div in divs if "复制" in div.text]
-                if target_divs:
-                    last_target_div = target_divs[-1]
-                driver.execute_script("arguments[0].scrollIntoView(true);",  last_target_div)
-                last_target_div.click()
-            except Exception as e:
-                print(Fore.RED + '复制时出错:' + str(e) + Style.RESET_ALL)
-                driver.refresh()
-                #配置AI
-                try:
-                    configure_AI(driver, AI_URL)
-                except Exception as e:
-                    print(Fore.RED + '配置AI时出错:' + str(e) + Style.RESET_ALL)
-                continue
+        copy_code(driver = driver, AI_URL = AI_URL)
         sleep(0.5)
         code = pyperclip.paste()
 
@@ -375,7 +345,7 @@ def problem_code() -> None:
             'pid': pid, 
             'tid': None
         }
-        response = requests.post(url='http://example.oj/api/submit-problem-judge', headers=headers, data=dumps(data))
+        response = requests.post(url=f"{user_data['OJ']['URL']}/api/submit-problem-judge", headers=headers, data=dumps(data))
         if response.status_code != 200:
             print('提交请求异常,status:' + str(response.status_code))
             driver.quit()
@@ -388,12 +358,12 @@ def problem_code() -> None:
 def get_user_data() -> None:
     global user_data_path, options, user_data
     user_data = {
-        'OJ': {'username': None, 'password': None}, 
+        'OJ': {'URL': None, 'username': None, 'password': None}, 
         'AI_cookies': None, 
         'AI_URL': None, 
         'ChromeDriver_path': None
     }
-    user_data['OJ']['username'], user_data['OJ']['password'], user_data['AI_URL'], user_data['ChromeDriver_path'] = input('OJ用户名:'), input('OJ密码:'), input('AI对话地址:'), input('ChromeDriver路径:')
+    user_data['OJ']['URL'], user_data['OJ']['username'], user_data['OJ']['password'], user_data['AI_URL'], user_data['ChromeDriver_path'] = input('OJ网址:').rstrip('/'), input('OJ用户名:'), input('OJ密码:'), input('AI对话地址:'), input('ChromeDriver路径:')
     driver = webdriver.Chrome(service=Service(user_data['ChromeDriver_path']),  options = options)
     print('请自行操作登录AI，完成后回车', end='')
     driver.get('https://bot.n.cn/')
@@ -413,7 +383,7 @@ if __name__ == '__main__':
     mode = None
     print(Fore.GREEN + '启动时间:' + str(int((time() - Start_time) * 100) / 100))
     while(mode != '4'):
-        print(Fore.BLUE + '由一只蚊子WZ制作' + Back.RED + Fore.WHITE + '\n仅供参考学习!' + Style.RESET_ALL + Fore.GREEN + '\n\n请选择模式:' + Fore.CYAN + '\n1.配置信息\n2.刷训练题目\n3.刷个题\n4.退出\n' + Style.RESET_ALL)
+        print(Fore.BLUE + '由一只蚊子WZ制作\nGitee仓库:https://gitee.com/wzokee/oj-auto-problem-solver-bot' + Back.RED + Fore.WHITE + '\n仅供参考学习!' + Style.RESET_ALL + Fore.GREEN + '\n\n请选择模式:' + Fore.CYAN + '\n1.配置信息\n2.刷训练题目\n3.刷个题\n4.退出\n' + Style.RESET_ALL)
         mode = input('请输入序号:')
         system('cls')
         if mode == '1':
