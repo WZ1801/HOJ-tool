46a77ab5064e46ac9175ec267d28c2657f370169
EchoSearch <1447998829@qq.com>
2025-08-02 15:49:46 +0800

    优化日志发送功能，增强封禁功能的用户反馈

--- module/ban_account.py
+++ module/ban_account.py
@@ -4,6 +4,41 @@ from os import system
 from json import load
 import requests, sys
 
+def send_log(type: str = 'info', message: str = '', mode: int=1) -> None:
+    try:
+        match type:
+            case 'info':
+                match mode:
+                    case 1:
+                        print(message)
+                    case 2:
+                        requests.post('http://127.0.0.1:1146/api/ban_account/log', json={'type': 'info', 'message': message})
+            case 'success':
+                match mode:
+                    case 1:
+                        print(Fore.GREEN + message + Style.RESET_ALL)
+                    case 2:
+                        requests.post('http://127.0.0.1:1146/api/ban_account/log', json={'type': 'success', 'message': message})
+            case 'warning':
+                match mode:
+                    case 1:
+                        print(Fore.YELLOW + message + Style.RESET_ALL)
+                    case 2:
+                        requests.post('http://127.0.0.1:1146/api/ban_account/log', json={'type': 'warning', 'message': message})
+            case 'error':
+                match mode:
+                    case 1:
+                        print(Fore.RED + message + Style.RESET_ALL)
+                    case 2:
+                        requests.post('http://127.0.0.1:1146/api/ban_account/log', json={'type': 'error', 'message': message})
+            case 'debug':
+                match mode:
+                    case 1:
+                        print(Fore.BLUE + message + Style.RESET_ALL)
+                    case 2:
+                        requests.post('http://127.0.0.1:1146/api/ban_account/log', json={'type': 'debug', 'message': message})
+    except: return
+
 # 初始化颜色
 init(autoreset=True)
 
@@ -19,12 +54,12 @@ except FileNotFoundError:
     user_data = None
 
 # 封号函数
-def ban(username):
+def ban(username, log_mode=1) -> None:
     for i in range(25):
         if login(username, 'esnb') == '对不起！登录失败次数过多！您的账号有风险，半个小时内暂时无法登录！':
-            print(f"已封禁:{username}")
+            send_log(type='success', message=f"{username}封禁成功！", mode=log_mode)
             return
-    print(f"{Fore.YELLOW}{username}为空号！{Style.RESET_ALL}")
+    send_log(type='warning', message=f"{username}可能是空号号不存在或平台禁用封禁。", mode=log_mode)
     
 
 def login(username, password):
@@ -44,53 +79,14 @@ def login(username, password):
 
     return response.json()['msg']
 
-# def main():
-#     if is_user_data_read:
-#         try:
-#             while True:
-#                 username = input('封禁账号名称,all为所有账号,Ctrl+C退出: ')
-#                 if username == 'all':
-#                     headers = {
-#                         'Accept': 'application/json',
-#                         'Content-Type': 'application/json', 
-#                         'Url-Type': 'general', 
-#                         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
-#                     } 
-#                     response = requests.get(f'{url}/api/get-rank-list?currentPage=1&limit=114514&type=1',  headers=headers)
-                    
-#                     # 检查响应状态码 
-#                     if response.status_code  != 200: 
-#                         print(f"{Fore.RED}请求失败，状态码：{response.status_code}{Style.RESET_ALL}")
-#                         continue 
-                    
-#                     try:
-#                         json_data = response.json() 
-#                     except requests.exceptions.JSONDecodeError: 
-#                         print(f"{Fore.RED}JSON解析失败。原始响应：{response.text}{Style.RESET_ALL}") 
-#                         continue 
-                    
-#                     # 检查JSON结构 
-#                     if 'data' not in json_data or 'records' not in json_data['data']:
-#                         print(f"{Fore.RED}响应JSON结构不符合预期：{json_data}{Style.RESET_ALL}")
-#                         continue 
-                    
-#                     for i in json_data['data']['records']: 
-#                         ban(i['username'])
-#                 else:
-#                     for _ in username.split(','):
-#                         ban(_)
-#         except KeyboardInterrupt:
-#             pass
-#         except Exception as e:
-#             print(Fore.RED + f'啊？这都报错:{e}' + Style.RESET_ALL)
-#     else:
-#         print(Fore.RED + '请先配置用户数据。' + Style.RESET_ALL)
-#     print(Fore.GREEN + '已退出' + Style.RESET_ALL)
-#     system('pause')
-
-def ban_account(mode: str) ->  None:
+def ban_account(mode: str, arg=None) ->  None:
     if mode == 'all':
-        white_list = input('请输入封禁白名单，以逗号分隔: ').split(',')
+        if arg == None:
+            log_mode = 1
+            white_list = input('请输入封禁白名单，以逗号分隔: ').split(',')
+        else:
+            log_mode = 2
+            white_list = arg.split(',')
         headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json', 
@@ -100,29 +96,37 @@ def ban_account(mode: str) ->  None:
         response = requests.get(f'{url}/api/get-rank-list?currentPage=1&limit=114514&type=1',  headers=headers)
         
         # 检查响应状态码 
-        if response.status_code  != 200: 
-            print(f"{Fore.RED}获取排行榜请求失败，状态码：{response.status_code}{Style.RESET_ALL}")
+        if response.status_code  != 200:
+            send_log(type='error', message=f"获取排行榜请求失败，状态码：{response.status_code}", mode=log_mode)
             return
         
         try:
             json_data = response.json() 
-        except requests.exceptions.JSONDecodeError: 
-            print(f"{Fore.RED}排行榜请求JSON解析失败。原始响应：{response.text}{Style.RESET_ALL}") 
+        except requests.exceptions.JSONDecodeError:
+            send_log(type='error', message=f"排行榜请求JSON解析失败。原始响应：{response.text}", mode=log_mode)
             return
         
         # 检查JSON结构 
         if 'data' not in json_data or 'records' not in json_data['data']:
-            print(f"{Fore.RED}获取排行榜响应JSON结构不符合预期：{json_data}{Style.RESET_ALL}")
+            send_log(type='error', message=f"获取排行榜响应JSON结构不符合预期：{json_data}", mode=log_mode)
             return
         
         for i in json_data['data']['records']: 
             if i['username'] in white_list:
                 continue
-            ban(i['username'])
+            ban(i['username'], log_mode)
     elif mode == 'assign':
-        username = input('封禁账号名称，用逗号分隔:').split(',')
-        for i in username: 
-            ban(i)
+        if arg == None:
+            username = input('封禁账号名称，用逗号分隔:').split(',')
+            log_mode = 1
+        else:
+            username = arg.split(',')
+            log_mode = 2
+            
+        send_log(type='info', message=f"开始封禁指定用户：{', '.join(username)}", mode=log_mode)
+        
+        for i in username:
+            ban(i, log_mode)
 
 def main() -> None:
     if is_user_data_read:
